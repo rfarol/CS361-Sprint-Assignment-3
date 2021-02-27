11ff867471ccfeb7123cab3cbcfea91ddf58ad86
rfarol <farolr@oregonstate.edu>
2021-02-27 15:15:44 -0800

    content generator wiki search

--- content-generator.py
+++ content-generator.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter.filedialog import askopenfile
 
 
+
 root = tk.Tk()
 def search_wiki(primary, secondary):
     """search_wiki function takes a primary and secondary argument and uses the primary argument to search a Wikipedia
@@ -23,41 +24,30 @@ def search_wiki(primary, secondary):
     paragraph_list = [] # intialize empty list
     for paragraph in soup.find_all('p'): # separate paragrpahs and add to list
         paragraph_list.append(paragraph)
-    if len(paragraph_list) == 0: # if no match was found, return output
-        result = "Page not found. Please search for another Primary word"
-        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
-        text_box.insert(1.0, result)
-        text_box.grid(column=1, row=7)
-        # download file button
-        download_button = tk.Button(root, text="Download CSV", font="Arial", bg="orange", height=1, width=12)
-        download_button.grid(column=1, row=8)
-    else:
-        prim_lower = primary.lower()
-        for search in paragraph_list: # search for paragraph with primary and secondary word
-            if prim_lower and secondary in search.text:
-                result = search.text # if both found, return output
-                if result == prim_lower:
-                    result = "Secondary word cannot be found. Please search for another Secondary word OR make a brand new search"
-                text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
-                text_box.insert(1.0, result)
-                text_box.grid(column=1, row=7)
-                # download file button
-                download_button = tk.Button(root, text="Download CSV", command=lambda:outputCSV(), font="Arial", bg="orange", height=1, width=12)
-                download_button.grid(column=1, row=8)
-                def outputCSV():
-                    """This function outputs the result into a csv file if the download button is pressed"""
-                    headers = []
-                    headers.append("input_keywords")
-                    headers.append("output_content")
-                    row = []
-                    row.append(primary + ";" + secondary)
-                    row.append(result)
-                    outfile = "output.csv"
-                    with open(outfile, "w") as csvfile:  # output a csv file containing new output
-                        csv_write = csv.writer(csvfile)
-                        csv_write.writerow(headers)  # output header
-                        csv_write.writerow(row)  # output row
-
+    prim_lower = primary.lower()
+    sec_lower = secondary.lower()
+    for search in paragraph_list: # search for paragraph with primary and secondary word
+        if prim_lower and sec_lower in search.text.lower():
+            result = search.text # if both found, return output
+            text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+            text_box.insert(1.0, result)
+            text_box.grid(column=1, row=7)
+            # download file button
+            download_button = tk.Button(root, text="Download CSV", command=lambda:outputCSV(), font="Arial", bg="orange", height=1, width=12)
+            download_button.grid(column=1, row=8)
+            def outputCSV():
+                """This function outputs the result into a csv file if the download button is pressed"""
+                headers = []
+                headers.append("input_keywords")
+                headers.append("output_content")
+                row = []
+                row.append(primary + ";" + secondary)
+                row.append(result)
+                outfile = "contentGenoutput.csv"
+                with open(outfile, "w") as csvfile:  # output a csv file containing new output
+                    csv_write = csv.writer(csvfile)
+                    csv_write.writerow(headers)  # output header
+                    csv_write.writerow(row)  # output row
 
 def getCSV():
     """getCSV function reads a CSV file that is uploaded from the user and calls search_wiki function once the primary
@@ -65,14 +55,38 @@ def getCSV():
     file = askopenfile(parent=root, mode="r", title="Choose a file", filetype=[("Csv file", "*.csv")])
     if file is not None:
         headers = []  # empty list for headers
-        final = []  # final output for rows
         csv_read = csv.reader(file)  # read csv file
         headers = next(csv_read)  # input headers to the empty list
-        headers.append("output_content")
+        if len(headers) > 2:
+            temp_1 = []
+            temp_2 = []
+            primary_list = []
+            secondary_list = []
+            for columns in csv_read:
+                temp_1.append(columns[2])
+                temp_2.append(columns[4])
+            for item in temp_1:
+                separate = item.split(" > ")
+                primary = separate[1]
+                primary_list.append(primary)
+            for item in temp_2:
+                secondary = item.split()
+                secondary_list.append(secondary)
+            secondary_revised = [item for sublist in secondary_list for item in sublist]
+            prim_counter = 0
+            sec_counter = 0
+            while prim_counter <= len(primary_list)-1:
+                result = search_wiki(primary_list[prim_counter], secondary_revised[sec_counter])
+                sec_counter += 1
+                if sec_counter <= len(secondary_revised):
+                    prim_counter += 1
+                    sec_counter = 0
+                if result != None:
+                    break
         for row in csv_read:  # iterate through each list
             for words in row:  # iterate through each element of the list
                 separate = words.split(";")  # split the two words
-                result = search_wiki(separate[0], separate[1])  # perform search on 1st and 2nd word of each list
+                result = search_wiki(separate[0], separate[1])  # perform search on 1st and 2nd word of each list"""
 
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3, rowspan=8)
