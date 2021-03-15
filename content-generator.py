46ca577e4894f2e12749e02897a81f62ed8bb810
rfarol <farolr@oregonstate.edu>
2021-03-14 18:35:21 -0700

    content generator wiki search

--- content-generator.py
+++ content-generator.py
@@ -34,11 +34,11 @@ def outputcontent(paragraphs, primary, secondary):
             result = search.text  # if both found, return output
             text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
             text_box.insert(1.0, result)
-            text_box.grid(column=1, row=9)
+            text_box.grid(column=1, row=11)
             # download file button
             download_button = tk.Button(root, text="Download CSV", command=lambda: outputCSV(primary, secondary, result), font="Arial",
                                         bg="orange", height=1, width=12)
-            download_button.grid(column=1, row=10)
+            download_button.grid(column=1, row=12)
 
 def outputCSV(primary, secondary, result):
     """This function outputs the result into a csv file if the download button is pressed"""
@@ -82,20 +82,10 @@ def readLifeCSV(csv_read):
     # store empty temp list
     temp_1 = []
     temp_2 = []
-    # store lists for search words
-    primary_list = []
-    secondary_list = []
-    for columns in csv_read: # take column 3 and 4 from the output to use for serach
+    for columns in csv_read:  # take column 3 and 4 from the output to use for serach
         temp_1.append(columns[2])
         temp_2.append(columns[4])
-    # format both columns and store into lists
-    for item in temp_1:
-        separate = item.split(" > ")
-        primary = separate[1]
-        primary_list.append(primary)
-    for item in temp_2:
-        secondary = item.split()
-        secondary_list.append(secondary)
+    primary_list, secondary_list = readLifeCSVhelper(temp_1, temp_2)
     secondary_revised = [item for sublist in secondary_list for item in sublist]
     prim_counter = 0
     sec_counter = 0
@@ -109,43 +99,67 @@ def readLifeCSV(csv_read):
         if result != None:
             break
 
+def readLifeCSVhelper(temp_1, temp_2):
+    """formats the life generator list for a more iterable form"""
+    # store lists for search words
+    primary_list = []
+    secondary_list = []
+    # format both columns and store into lists
+    for item in temp_1:
+        separate = item.split(" > ")
+        primary = separate[1]
+        primary_list.append(primary)
+    for item in temp_2:
+        secondary = item.split()
+        secondary_list.append(secondary)
+    return primary_list, secondary_list
+
+
 canvas = tk.Canvas(root, width=600, height=300)
-canvas.grid(columnspan=3, rowspan=10)
+canvas.grid(columnspan=3, rowspan=12)
 
 # instructions 1
-instructions_1 = tk.Label(root, text="Look Me Up!", font="Arial")
+instructions_1 = tk.Label(root, text="Look Me Up!", font="Arial", fg="orange", width="100")
 instructions_1.grid(column=1, row=0)
 
 # instructions 2
-instructions_2 = tk.Label(root, text="Please enter a primary and secondary search word below!", font="Arial")
+instructions_2 = tk.Label(root, text="Want to make a search on your favorite topic? Enter two words below!", font="Arial")
 instructions_2.grid(column=1, row=1)
+
+# instructions 3
+instructions_3 = tk.Label(root, text="The first word will search the Wikipedia page. The second word will search a paragraph containing both words", font="Arial")
+instructions_3.grid(column=1, row=2)
 
 # primary entry
 p_entry = tk.Entry(root, font=40)
-p_entry.grid(column=1, row=2)
+p_entry.grid(column=1, row=3)
 
 # secondary entry
 s_entry = tk.Entry(root, font=40)
-s_entry.grid(column=1, row=3)
+s_entry.grid(column=1, row=4)
 
 # generate button
 generate_button = tk.Button(root, text="Generate Search", font=50, bg="orange", command=lambda: search_wiki(str(p_entry.get()), str(s_entry.get())))
-generate_button.grid(column=1, row=4)
+generate_button.grid(column=1, row=5)
 
-#instructions 3
-instructions_3 = tk.Label(root, text="OR you can input your own CSV file!", font="Arial")
-instructions_3.grid(column=1, row=5)
+#instructions 5
+instructions_5 = tk.Label(root, text="OR you can input your own CSV file!", font="Arial")
+instructions_5.grid(column=1, row=6)
 
-# instructions 4
-instructions_2 = tk.Label(root, text="The CSV file must contain a header and columns containing TWO search words", font="Arial")
-instructions_2.grid(column=1, row=6)
+# instructions 6
+instructions_6 = tk.Label(root, text="The CSV file must contain a header and columns containing TWO search words", font="Arial")
+instructions_6.grid(column=1, row=7)
 
-# instructions 5
-instructions_2 = tk.Label(root, text='Please separate both search words with a ";"', font="Arial")
-instructions_2.grid(column=1, row=7)
+# instructions 7
+instructions_7 = tk.Label(root, text='Please separate both search words with a ";"', font="Arial")
+instructions_7.grid(column=1, row=8)
 
 # search file button
 select_button = tk.Button(root, text="Search File", command=lambda:getCSV(), font="Arial", bg="orange", height=1, width=10)
-select_button.grid(column=1, row=8)
+select_button.grid(column=1, row=9)
+
+# instructions 8
+instructions_8 = tk.Label(root, text="NOTE: Life Generator output is compatible. Please use CSV file in order to search", font="Arial")
+instructions_8.grid(column=1, row=10)
 
 root.mainloop()
